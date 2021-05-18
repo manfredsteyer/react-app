f413ac21a64b3099a77ce3c672d46230917da1b4
Manfred Steyer <manfred.steyer@gmx.net>
2021-05-18 09:37:49 +0200

    done

---
+++ app.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+
+  render() {
+    const reactVersion = require('./package.json').dependencies['react'];
+
+    return ([
+        <h1>
+          Micro Frontend 4
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="30"></img>
+        </h1>,
+        <p>
+          React Version: {reactVersion}
+        </p>
+    ])
+  }
+}
+
+class Mfe4Element extends HTMLElement {
+  connectedCallback() {
+    ReactDOM.render(<App/>, this);
+  }
+}
+
+customElements.define('mfe4-element', Mfe4Element);
