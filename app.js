f3ba2ce31b270a7404b7c0292076c9ac5fef5f72
Manfred Steyer <manfred.steyer@gmx.net>
2022-01-27 23:23:32 +0100

    feat: provide version not using web components

--- app.js
+++ app.js
@@ -6,22 +6,42 @@ class App extends React.Component {
   render() {
     const reactVersion = require('./package.json').dependencies['react'];
 
-    return ([
+    return (
+      <div>
         <h1>
           React
           <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="30"></img>
-        </h1>,
+        </h1>
         <p>
           React Version: {reactVersion}
         </p>
-    ])
+      </div>
+    )
   }
 }
 
-class Mfe4Element extends HTMLElement {
-  connectedCallback() {
-    ReactDOM.render(<App/>, this);
-  }
+// class Mfe4Element extends HTMLElement {
+//   connectedCallback() {
+//     console.log('ReactDOM.render');
+//     ReactDOM.render(<App/>, this);
+//   }
+// }
+
+// customElements.define('react-element', Mfe4Element);
+
+export function bootstrap() {
+  console.log('bootstrap');
+  ReactDOM.render(
+    <App/>, 
+    document.querySelector('react-element')
+  );
 }
 
-customElements.define('react-element', Mfe4Element);
+export function destroy() {
+  console.log('destroy');
+  ReactDOM.unmountComponentAtNode(
+    document.querySelector('react-element')
+  )
+}
+
+// bootstrap();
